e539db08c26e2622296e2a76660ae6622d81de8c
samplesolutions23 <samplesolutions23@gmail.com>
2024-09-12 14:16:05 -0500

    Update apps.py

--- apps.py
+++ apps.py
@@ -4,7 +4,6 @@ import time
 import re
 import os
 import requests
-import env
 from youtube_transcript_api import YouTubeTranscriptApi
 from urllib.parse import urlparse
 from transformers import pipeline
